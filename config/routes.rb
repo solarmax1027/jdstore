61bac4147d9011344b50fe616442c02452d3d13b
solarmax <solarmax1027@gmail.com>
2017-03-02 19:45:25 +0800

    add ship/shiping/cancel/reture action to admin/orders_controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,17 @@ Rails.application.routes.draw do
 
   namespace :admin do
      resources :products
+     resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
    end
+
+     
 
 
 
@@ -21,7 +31,7 @@ Rails.application.routes.draw do
       end
 
     resources :cart_items
-     resources :orders
+
 
     resources :orders do
     member do
@@ -33,6 +43,8 @@ Rails.application.routes.draw do
   namespace :account do
         resources :orders
   end
+
+
 
 
   root 'products#index'
